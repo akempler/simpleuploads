5d6c96799825a47c74e76dbc5817f74717ef56e8
Adam Kempler <akempler@gmail.com>
2015-01-05 15:41:18 -0500

    initial commit

---
+++ js/simpleuploads.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * JavaScript to place the content palette on the screen 
+ * and synchronize settings between the palette and the node form.
+ */
+
+
+jQuery(document).ready(function() {
+
+  if (typeof CKEDITOR !== 'undefined') {
+
+    CKEDITOR.on('instanceReady', function(e) {
+      var editor = e.editor;
+
+      // When the upload has finished the plugin has finished
+      // and the element is ready on the page.
+      editor.on('simpleuploads.finishedUpload' , function(ev) {
+        var element = ev.data.element;
+        element.addClass("img-responsive");
+
+        element.removeAttribute("width");
+        element.removeAttribute("height");
+      });
+    });
+  }
+});
